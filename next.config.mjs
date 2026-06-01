bcbc1dbe8b950d1522a1f002fb41a6dc36c31090
Bruno Pixels <twaggbru@gmail.com>
2026-06-01 15:58:44 +0200

    Added gitignore and the mjs file

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any other specific configurations your app needs here
+};
+
+export default nextConfig;
